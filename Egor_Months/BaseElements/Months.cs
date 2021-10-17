9a3710925696c20ea610d3ad08d3ccddb1c4bf0c
Wendermark <caseharo.e@gmail.com>
2021-10-17 08:00:46 +0300

    Добавьте файлы проекта.

---
+++ Egor_Months/BaseElements/Months.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egor_Months.BaseElements
+{
+    enum Months
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class Month
+    {
+        public static void GetById(int id)
+        {
+            if (id < 1 || id > 12)
+            {
+                Console.WriteLine("Такого месяца не существует!");
+                return;
+            }
+
+            Console.WriteLine($"Месяц - { (Months)id } Кол-во дней - {DateTime.DaysInMonth(DateTime.Now.Year, id)}");
+        }
+
+        public static void GetByDays(int days)
+        {
+            if (days < 28 || days > 31 || days.Equals(29))
+            {
+                Console.WriteLine("Таких месяцев не существует!");
+                return;
+            }
+
+            for (Months i = Months.January; i < Months.December; i++)
+            {
+                if (DateTime.DaysInMonth(DateTime.Now.Year, (int)i).Equals(days))
+                    Console.WriteLine(i.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Egor_Months/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Egor_Months.BaseElements;
+
+namespace Egor_Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+
+            int.TryParse(Console.ReadLine(), out int id);
+
+            Month.GetById(id);
+
+            Console.WriteLine("Введите кол-во дней в месяце");
+
+            int.TryParse(Console.ReadLine(), out int days);
+
+            Month.GetByDays(days);
+
+            Console.ReadKey();
+        }
+    }
+}
